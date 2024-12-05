b8aa471b26630316499c9acada1d3bc99bebb0e8
“Alice0509 <soojin527@gamil.com>
2024-12-05 21:29:21 +0100

    Initial commit: 프로젝트 현재 상태 백업

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,12 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // 'transform-react-jsx-self', // 제거
-      // 'transform-react-jsx-source', // 제거
+      ['module:react-native-dotenv', {
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true,
+      }],
       // 다른 플러그인들...
     ],
   };
